0c6ece57d65efe196da6da34e9f7f8197118d698
Penlika <superlaferrari@gmail.com>
2024-09-26 21:41:47 +0700

    ok

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,52 +1,83 @@
-//Project 1
+import { useState } from "react"
+import { Image, ImageBackground, StyleSheet, Text, TextInput, TouchableHighlight, View } from "react-native"
 
-// import { View } from "react-native";
-// import Cat1, {f1,pi} from "./Components/Cat";
-// import Project1 from "./Components/Project1";
-
-// const App= ()=>{
-//   console.log(pi)
-//   console.log(f1(25))
-//   return(
-//     <View>
-//       <Project1></Project1>
-//     </View>
-//   )
-// }
-//  export default App;
-
-//Project 2
-
-import { StyleSheet, View } from "react-native"
-import Project1 from "./Components/Project1";
-import Project2 from "./Components/Project2"
-import Project3 from "./Components/PRoject3";
-import Project4 from "./Components/Project4";
-import Project5 from "./Components/Project5";
-import Project6 from "./Components/Project6";
-import Project7 from "./Components/Project7";
-import Project8 from "./Components/Project8";
-
-const App= ()=>{
-    return(
-        <View style={myStyle.container}>
-          {/* <Project1></Project1> */}
-          {/* <Project2></Project2> */}
-          {/* <Project3></Project3> */}
-          {/* <Project4></Project4> */}
-          {/* <Project5></Project5> */}
-          {/* <Project6></Project6> */}
-          {/* <Project7></Project7> */}
-          {/* <Project8></Project8> */}
-        </View>
-    )
-}
-export default App;
-
-const myStyle = StyleSheet.create(
-  {
-    container:{
-      flex:1
-    }
+const App=()=>{
+  const[username, setUserName]= useState("sadasdadas")
+  const[count, setCount]=useState(0)
+  const onPress=()=>{
+    setCount(count+1)
   }
-)
+  return(
+    <View style={myStyle.container}>
+      {/* <View style={{...myStyle.box,backgroundColor:"red"}}>
+
+      </View>
+      <View style={{...myStyle.box,backgroundColor:"green"}}>
+
+      </View>
+      <View style={{...myStyle.box,backgroundColor:"blue"}}>
+
+      </View> */}
+
+      {/* <TextInput
+        style={{height:50,width:"100%",borderColor:"blue",borderWidth:1}}
+        onChangeText={setUserName}
+        //<=>onChangeText={(text)=>setUserName(text)}
+        value={username}
+        keyboardType="default"
+        placeholder="Gà"
+        secureTextEntry={true}
+      /> */}
+
+      {/* <Image
+        //source={require("./image/Screenshop.png")}
+        source={{uri:"https://www.drlsdirect.com/cdn/shop/files/BMW3SeriesF30-YellowHeadlight_DRLsDirect.com.png?v=1685722748&width=750"}}
+        style={{flex:1,width: 400, height: 400}}
+      /> */}
+
+      {/* <ImageBackground
+      source={require("./image/Screenshop.png")}
+      style={{flex:1}}>
+        <Text style={{fontSize:30, color:"red"}}>
+        Hello
+        </Text>
+
+      </ImageBackground> */}
+
+      <Text>Count: {count}</Text>
+      <TouchableHighlight
+      style={myStyle.button}
+      onPress={onPress}>
+        <Text style={myStyle.buttonText}>Press me</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+export default App
+
+const myStyle=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"yellow",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  box:{
+    height:100,
+    width:100
+  },
+  button:{
+    height:50,
+    width:"90%",
+    borderRadius:95,
+    backgroundColor:"aqua",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  buttonText:{
+    fontSize:20,
+    fontWeight:"bold"
+  }
+})
